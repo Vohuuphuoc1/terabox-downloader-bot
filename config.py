2168e04a3c1357b2dcece4f714e35d086b87c7f2
Vohuuphuoc1 <159933437+Vohuuphuoc1@users.noreply.github.com>
2026-06-09 08:39:41 +0700

    Update API credentials and configuration settings

--- config.py
+++ config.py
@@ -1,23 +1,23 @@
-API_ID = 123456  # api id
-API_HASH = "ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # api hash
+API_ID = 35935694  # api id
+API_HASH = "270fc7395c68e32a907b0baaf910700c"  # api hash
 
-BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # bot token
+BOT_TOKEN = "8678869575:AAGgF-LGcw0UP3d6YB3D7K3EaIqBcS5NhDs"  # bot token
 
 
 # REDIS
-HOST = "localhost"  # redis host uri
+HOST = "red-d8jli5sm0tmc73fej0jg"  # redis host uri
 PORT = 6379  # redis port
 PASSWORD = ""  # redis password
 
-PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
+PRIVATE_CHAT_ID = -1003998476130  # CHAT WHERE YOU WANT TO STORE VIDEOS
 # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=;
 COOKIE = ""
-ADMINS = [1317173146]
+ADMINS = [5575707907]
 
 
-BOT_USERNAME = "teraboxdown_bot"
+BOT_USERNAME = "phuoc_download_bot"
 
 # Force user to join this channel. (make sure you have promoted the bot on this chat.)
-FORCE_LINK = "@RoldexVerse"
+FORCE_LINK = ""
 
 PUBLIC_EARN_API = ""  # https://publicearn.com/api
