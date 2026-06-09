29c9607cc865555900f4e8466a6a6a27c03683de
Vohuuphuoc1 <159933437+Vohuuphuoc1@users.noreply.github.com>
2026-06-09 08:54:14 +0700

    Cập nhật file config.py để sử dụng các biến môi trường.
    Refactor configuration to use environment variables for sensitive data.

--- config.py
+++ config.py
@@ -1,23 +1,8 @@
-API_ID = 35935694  # api id
-API_HASH = "270fc7395c68e32a907b0baaf910700c"  # api hash
+import os
 
-BOT_TOKEN = "8678869575:AAGgF-LGcw0UP3d6YB3D7K3EaIqBcS5NhDs"  # bot token
-
-
-# REDIS
-HOST = "redis://red-d8jli5sm0tmc73fej0jg"  # redis host uri
-PORT = 6379  # redis port
-PASSWORD = ""  # redis password
-
-PRIVATE_CHAT_ID = -1003998476130  # CHAT WHERE YOU WANT TO STORE VIDEOS
-# COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=;
-COOKIE = ""
-ADMINS = [5575707907]
-
-
-BOT_USERNAME = "phuoc_download_bot"
-
-# Force user to join this channel. (make sure you have promoted the bot on this chat.)
-FORCE_LINK = ""
-
-PUBLIC_EARN_API = ""  # https://publicearn.com/api
+API_ID = int(os.environ.get("API_ID", 35935694))
+API_HASH = os.environ.get("API_HASH", "270fc7395c68e32a907b0baaf910700c")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8678869575:AAGgF-LGcw0UP3d6YB3D7K3EaIqBcS5NhDs")
+REDIS_URL = os.environ.get("REDIS_URL", "redis://red-d8jli5sm0tmc73fej0jg:6379")
+PRIVATE_CHAT_ID = int(os.environ.get("PRIVATE_CHAT_ID", -1003998476130))
+ADMINS = [int(x) for x in os.environ.get("ADMINS", "5575707907").split()]
