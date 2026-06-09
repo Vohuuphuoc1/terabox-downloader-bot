90a6e6835389e57a6a58a23444838d96bb42be55
Vohuuphuoc1 <159933437+Vohuuphuoc1@users.noreply.github.com>
2026-06-09 08:56:50 +0700

    Thêm cấu hình Redis vào config.py
    Added Redis configuration for bot functionality.

--- config.py
+++ config.py
@@ -3,6 +3,12 @@ import os
 API_ID = int(os.environ.get("API_ID", 35935694))
 API_HASH = os.environ.get("API_HASH", "270fc7395c68e32a907b0baaf910700c")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8678869575:AAGgF-LGcw0UP3d6YB3D7K3EaIqBcS5NhDs")
+
+# Thêm lại các dòng này để bot không bị lỗi Import
 REDIS_URL = os.environ.get("REDIS_URL", "redis://red-d8jli5sm0tmc73fej0jg:6379")
+HOST = "red-d8jli5sm0tmc73fej0jg"
+PORT = 6379
+PASSWORD = ""
+
 PRIVATE_CHAT_ID = int(os.environ.get("PRIVATE_CHAT_ID", -1003998476130))
 ADMINS = [int(x) for x in os.environ.get("ADMINS", "5575707907").split()]
